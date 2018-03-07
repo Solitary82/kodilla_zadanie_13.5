c1e44f7d227d4e83e2e9a65573c02adbdb8cfa78
Marta Waplak <mwaplak@gmail.com>
2018-03-07 15:12:51 +0100

    Upload all

---
+++ modules/OSinfo.js
@@ -0,0 +1,23 @@
+var os = require('os');
+var timeFormat = require('../modules/timeformat');
+
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('Sysytem: ', type);
+    console.log('Release ', release);
+    console.log('CPU model: ', cpu);
+    console.log('Uptime: ', timeFormat.print(uptime));
+    console.log('User name: ', userInfo.username);
+    console.log('Home dir: ', userInfo.homedir);
+}
+
+exports.print = getOSinfo;
